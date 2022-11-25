230f7ea6420da1ba3f1333ee6c910e5ca85dcf85
dmunofer <diego.fmz@hotmail.com>
2022-11-25 23:49:28 +0100

    cambios

--- Ejercicio2/ej2.py
+++ Ejercicio2/ej2.py
@@ -42,6 +42,62 @@ def nombresElec():
 def ordenascendente():
     return df.iloc[::-1]
 
+def debilesJolteon():
+    lista1=df[df['Type 1']=='Water'].index.tolist()
+    lista2=df[df['Type 1']=='Fliying'].index.tolist()
+    lista5=df[df['Type 2']=='Water'].index.tolist()
+    lista6=df[df['Type 2']=='Fliying'].index.tolist()
+    lista3=df[df['Type 1']=='Ground'].index.tolist()
+    lista4=df[df['Type 2']=='Ground'].index.tolist()
+    lista=lista1+lista2-lista3-lista4+lista5+lista6
+    for indice in lista:
+        df.iloc[indice]
+
+def debilesLycanroc():
+    lista1=df[df['Type 1']=='Fire'].index.tolist()
+    lista2=df[df['Type 1']=='Fliying'].index.tolist()
+    lista3=df[df['Type 1']=='Bug'].index.tolist()
+    lista4=df[df['Type 1']=='Ice'].index.tolist()
+    lista5=df[df['Type 2']=='Fire'].index.tolist()
+    lista6=df[df['Type 2']=='Fliying'].index.tolist()
+    lista7=df[df['Type 2']=='Bug'].index.tolist()
+    lista8=df[df['Type 2']=='Ice'].index.tolist()
+    lista=lista1+lista2+lista3+lista4+lista5+lista6+lista7+lista8
+    for indice in lista:
+        df.iloc[indice]
+
+def debilesTyrantrum():
+    lista1=df[df['Type 1']=='Fire'].index.tolist()
+    lista2=df[df['Type 1']=='Fliying'].index.tolist()
+    lista3=df[df['Type 1']=='Bug'].index.tolist()
+    lista4=df[df['Type 1']=='Ice'].index.tolist()
+    lista5=df[df['Type 2']=='Fire'].index.tolist()
+    lista6=df[df['Type 2']=='Fliying'].index.tolist()
+    lista7=df[df['Type 2']=='Bug'].index.tolist()
+    lista8=df[df['Type 2']=='Ice'].index.tolist()
+    lista9=df[df['Type 1']=='Dragon'].index.tolist()
+    lista10=df[df['Type 1']=='Fairy'].index.tolist()
+    lista11=df[df['Type 2']=='Fairy'].index.tolist()
+    lista=lista1+lista2+lista3+lista4+lista5+lista6+lista7+lista8+lista9-lista10-lista11
+    for indice in lista:
+        df.iloc[indice]
+
+def mostrarTipos():
+    tipos=[]
+    for column in df.columns['Type 1']:
+        if column not in tipos:
+            tipos.append(column)
+    return tipos
+
+def mostrarPokeTipo(tipo):
+    lista1=df[df['Type 1']==str(tipo)].index.tolist()
+    lista2=df[df['Type 1']==str(tipo)].index.tolist()
+    lista=lista1+lista2
+    for indice in lista:
+        df.iloc[indice]
+
+
+
 
 
 
