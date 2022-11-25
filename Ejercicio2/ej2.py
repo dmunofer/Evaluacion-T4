649bc3722804b2c30b403535423f4cd8190e7e7e
dmunofer <diego.fmz@hotmail.com>
2022-11-25 23:24:58 +0100

    cambios

--- Ejercicio2/ej2.py
+++ Ejercicio2/ej2.py
@@ -2,3 +2,23 @@ import csv
 import pandas as pd
 
 df = pd.read_csv("Pokemon.csv")
+
+def mostrarPokenumero(npoke):
+    return df.loc[df['#']==npoke]
+
+def mostrarPokenombre(nombre):
+    lista=df[df['Name']==nombre].index.tolist()
+    for indice in lista:
+        df.iloc[indice]
+
+def nombresAgua():
+    lista1=df[df['Type 1']=='Water'].index.tolist()
+    lista2=df[df['Type 2']=='Water'].index.tolist()
+    lista=lista1+lista2
+    for indice in lista:
+        df.iloc[indice]
+
+
+
+
+
