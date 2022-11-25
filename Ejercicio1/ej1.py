ff926a54d94cf23cbc2e32ed4bb01ec2209d6596
dmunofer <diego.fmz@hotmail.com>
2022-11-25 22:18:29 +0100

    cambios

--- Ejercicio1/ej1.py
+++ Ejercicio1/ej1.py
@@ -13,4 +13,11 @@ def encriptado(datos,coding):
         encriptado.append(coding[dato])
     cadena=  ''.join([str(item) for item in encriptado])
     return cadena
-    
+
+def Huffman(datos):
+    simbolosconfrecs={"A":"00", "1":"01", "3":"100", "0":"1010", "M":"1011", "T":"110", "F":"111"}
+    simbolos= ["A", "F", "1", "3", "0", "M", "T"]
+    frecuencias= [0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15]
+    nodos=[]
+    for simbolo in simbolos:
+        nodos.append(Nodos(simbolosconfrecs.get(simbolo), simbolo))
