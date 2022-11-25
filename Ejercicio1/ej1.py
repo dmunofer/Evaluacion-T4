8547ce32308495450fae5d0a6b66533ba4b09b6e
dmunofer <diego.fmz@hotmail.com>
2022-11-25 22:09:25 +0100

    cambios

---
+++ Ejercicio1/ej1.py
@@ -0,0 +1,7 @@
+
+class Nodos():
+    def __init__(self,frecuencia,simbolo,left=None,right=None):
+        self.frecuencia=frecuencia
+        self.simbolo=simbolo
+        self.left=left
+        self.right=right
