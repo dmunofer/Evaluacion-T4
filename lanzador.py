97fbe1073417cfbf8fdfd928dcb1644cd95c307c
dmunofer <diego.fmz@hotmail.com>
2022-11-26 13:36:55 +0100

    cambios

--- lanzador.py
+++ lanzador.py
@@ -1,1 +1,13 @@
 import sys
+
+sys.path.insert(0,"/Users/smite/Documents/GitHub/EvaluacionT4/Ejercicio1/ej1")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/EvaluacionT4/Ejercicio2/ej2")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/EvaluacionT4/Ejercicio3/ej3")
+
+from Ejercicio1 import ej1 
+from Ejercicio2 import *
+from Ejercicio3 import ej3
+
+def exe():
+    ejercicio=input("Inserte el numero de el ejercicio que quieras ejecutar:")
+    
